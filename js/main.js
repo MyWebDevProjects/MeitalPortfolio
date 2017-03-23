0f53412a0618a5e067264a97f3371d8359af0b1f
yuda85 <yuda8855@gmail.com>
2017-03-22 23:59:29 -0600

    testing the new lightbox module

--- js/main.js
+++ js/main.js
@@ -3,58 +3,53 @@
 // This is The Scripts used for ___________ Theme
 //
 //
-
 function main() {
-
-(function () {
-   'use strict';
-
-   /*====================================
-    Main Navigation Stick to Top when Scroll
-    ======================================*/
-    function sticky_relocate() {
-      var window_top = $(window).scrollTop();
-      var div_top = $('#sticky-anchor').offset().top;
-      if (window_top > div_top) {
-          $('#tf-menu').addClass('stick');
-      } else {
-          $('#tf-menu').removeClass('stick');
-      }
-  }
-
-  $(function () {
-      $(window).scroll(sticky_relocate);
-      sticky_relocate();
-  });
-
-    
- $(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top - 70
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
-
-}());
+    (function () {
+        'use strict';
+        /*====================================
+         Main Navigation Stick to Top when Scroll
+         ======================================*/
+        function sticky_relocate() {
+            var window_top = $(window).scrollTop();
+            var div_top = $('#sticky-anchor').offset().top;
+            if (window_top > div_top) {
+                $('#tf-menu').addClass('stick');
+            }
+            else {
+                $('#tf-menu').removeClass('stick');
+            }
+        }
+        $(function () {
+            $(window).scroll(sticky_relocate);
+            sticky_relocate();
+        });
+        $(function () {
+            $('a[href*=#]:not([href=#])').click(function () {
+                if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                    var target = $(this.hash);
+                    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                    if (target.length) {
+                        $('html,body').animate({
+                            scrollTop: target.offset().top - 70
+                        }, 1000);
+                        return false;
+                    }
+                }
+            });
+        });
+    }());
 }
 main();
-
-$(document).ready(function(){
-    
-    $('.img-responsive').click(function(){
-        lightbox.option({
-            
-        });
-        
+$(document).ready(function () {
+    $('#img-winter').click(function () {
+        console.log("click works")
     });
+    var gallery = $('#img-winter').simpleLightbox();
+   
+    var img1 = $('#img-winter');
     
-    
+    $('#img-winter').click('open.simplelightbox', function () {
+        console.log("lightbox test")
+       gallery.open(img1);
+    });
 }); //end ready
